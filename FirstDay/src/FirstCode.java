091655657a6f140438426ba511789cc36764e011
fdg <fdg@fdg-PC>
2021-09-20 14:34:35 +0530

    first commit

---
+++ FirstDay/src/FirstCode.java
@@ -0,0 +1,24 @@
+
+public class FirstCode {
+
+	
+		private void javaCourse(){
+			System.out.println("Java");
+			
+		}
+		private void oracleCourse(){
+			System.out.println("Oracle");
+		}
+		private void seleniumCourse(){
+			System.out.println("Selenium");
+		}
+		public static void main(String[] args){
+			FirstCode firstcode = new FirstCode();
+			
+			firstcode.javaCourse();
+			firstcode.oracleCourse();
+			firstcode.seleniumCourse();
+		}
+	}
+
+
